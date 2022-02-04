a75acfe660deca01c5735207a13006696c545400
luan <kellyhangmy@gmail.com>
2022-02-04 10:51:08 +0700

    feat: install metamask on mobile

--- src/providers/connectors/noinjectedprovider.ts
+++ src/providers/connectors/noinjectedprovider.ts
@@ -1,7 +1,12 @@
-const NoInjectedProvider = async () => {
-    let provider = {name: "noindexedprovider", returnTrue: true, on: function(){}, off: function() {}};
+import { isMobile } from "../../helpers";
 
-  let win = window.open("https://metamask.io/download", "_blank") || window;
+const NoInjectedProvider = () => {
+  const mobile = isMobile();
+
+  let provider = {name: "noindexedprovider", returnTrue: true, on: function(){}, off: function() {}};
+
+  const url = mobile ? "https://metamask.app.link/dapp/${window.location.host}" : "https://metamask.io/download"
+  let win = window.open(url, "_blank") || window;
   win.focus();
 
   return provider;
